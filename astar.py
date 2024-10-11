38fd9cedfc3e755e92f2867728618568b7ac0894
SamPatatoz <issamissam@hotmail.be>
2024-10-11 18:09:42 +0200

    Added docs

--- astar.py
+++ astar.py
@@ -45,7 +45,14 @@ class PacmanAgent(Agent):
             return Directions.STOP
 
     def astar(self, state):
+        """Given a Pacman game state, returns a list of legal moves to solve
 
+        Args:
+            state: a game state. See API or class `pacman.GameState`.
+
+        Returns:
+            a list of legal moves
+        """
         closed = set()
         open_queue = PriorityQueue()
         open_queue.push((state, []), 0)
@@ -77,20 +84,39 @@ class PacmanAgent(Agent):
                     g_score[successor] = tentative_g_score
                     f_score = tentative_g_score + self.heuristic(successor.getPacmanPosition(), state.getFood())
                     
-                    new_path = path + [action]  # Add the action to the current path
+                    new_path = path + [action]
                     open_queue.push((successor, new_path), f_score)
                     
         # No solution
         return []
     
     def reconstruct_path(self, parent, current):
+        """Reconstruct the path from the parent dictionary
+
+        Args:
+            parent: dictionary containing the parent of each node
+            current: the current node
+
+        Returns:
+            the total path from the start to the current
+        """
         total_path = []
         while parent[current] is not None:
             total_path.insert(0, current)
             current = parent[current]
         return total_path
 
-    def heuristic(self, point1, food_grid):
+    def heuristic(self, pacman_pos, food_grid):
+        """find the heuristic value for the given pacman position and food grid
+
+        Args:
+            pacman_pos: position of pacman
+            food_grid: grid of food
+
+        Returns:
+            the heuristic value
+        """
         food_positions = food_grid.asList() 
         # get the manhattan distance to the closest
-        return min([manhattanDistance(point1, food) for food in food_positions])
+        #TODO: find better heuristic
+        return min([manhattanDistance(pacman_pos, food) for food in food_positions])
